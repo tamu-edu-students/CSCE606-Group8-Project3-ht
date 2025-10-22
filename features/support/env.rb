dfc90ed35ef9005e8c9f55e833bdb30a9a966126
mihirsanjay <mihirsanjay@gmail.com>
2025-10-22 16:41:05 -0500

    corrected OmniAuth mock route for Google login in ticket create feature

--- features/support/env.rb
+++ features/support/env.rb
@@ -52,3 +52,6 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
+Before do
+  login_with_google
+end
--- features/support/login_helper.rb
+++ features/support/login_helper.rb
@@ -1,14 +1,18 @@
-Before('@wip') do
-  # Simulate OmniAuth login manually instead of using set_rack_session
-  OmniAuth.config.test_mode = true
-  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-    provider: 'google_oauth2',
-    uid: '12345',
-    info: {
-      email: 'testuser@example.com',
-      name: 'Test User'
-    }
-  })
+module LoginHelper
+  def login_with_google
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+      provider: 'google_oauth2',
+      uid: '12345',
+      info: {
+        name: 'Test User',
+        email: 'testuser@example.com'
+      }
+    )
 
-  visit '/auth/google_oauth2'
+    # This matches your real route
+    visit '/auth/google_oauth2'
+  end
 end
+
+World(LoginHelper)
