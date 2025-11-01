63a96005b5bbc4430342bfbea019d2762d4730ff
Keegan Smith <keeganasmith2003@gmail.com>
2025-11-01 18:55:14 -0500

    attempt to pass brakeman

--- app/policies/team_membership_policy.rb
+++ app/policies/team_membership_policy.rb
@@ -1,4 +1,23 @@
+# app/policies/team_membership_policy.rb
 class TeamMembershipPolicy < ApplicationPolicy
-  def create? = user.admin?
-  def destroy? = user.admin?
+  def create?
+    user.sysadmin?
+  end
+
+  def update?
+    user.sysadmin?
+  end
+
+  def destroy?
+    user.sysadmin?
+  end
+
+  # Only sysadmins can set user_id and role via params
+  def permitted_attributes
+    if user.sysadmin?
+      [:user_id, :role]
+    else
+      [] # or [:user_id] if you allow self-join; never :role here
+    end
+  end
 end
